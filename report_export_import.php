897549920938a3a9915ed73cfe84db501f11de8c
kesiyaraj <kesiya.raj@sjsu.edu>
2021-05-02 15:16:22 -0700

    design

--- report_export_import.php
+++ report_export_import.php
@@ -13,7 +13,97 @@ $conn = new mysqli($servername, $username, $password, $db);
 $startdate = $_SESSION['startdate'] ;
 $enddate = $_SESSION['enddate'] ;
 
-
+echo "<head>
+					<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css\" rel=\"stylesheet\">
+						<title></title>
+						<style>
+							.centered {
+					  position: fixed;
+					  top: 50%;
+					  left: 50%;
+					  margin-top: -50px;
+					  margin-left: -100px;
+					}
+					.hero-image {
+					  background-image: url(\"/images/shipping.jpeg\");
+					  background-color: #cccccc;
+					  height: 180px;
+					  background-position: center;
+					  background-repeat: no-repeat;
+					  background-size: cover;
+					  position: relative;
+					}
+					
+					.hero-text {
+					  text-align: center;
+					  position: absolute;
+					  top: 50%;
+					  left: 50%;
+					  transform: translate(-50%, -50%);
+					  color: white;
+					}
+					.heading {
+						text-align: center;
+						background-color: cadetblue;
+						padding: 5px;
+						margin: 3px;
+						color: white;
+					}
+						</style>
+					</head>";
+					echo '<body style="overflow-x:none">
+					<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+							  <a class="navbar-brand" href="#">
+								Shipping Port
+							  </a>
+							  <button
+								class="navbar-toggler"
+								type="button"
+								data-toggle="collapse"
+								data-target="#navbarNav"
+								aria-controls="navbarNav"
+								aria-expanded="false"
+								aria-label="Toggle navigation"
+							  >
+								<span class="navbar-toggler-icon"></span>
+							  </button>
+							  <div class="collapse navbar-collapse" id="navbarNav">
+								<ul class="navbar-nav">
+								  <li class="nav-item active">
+									<a class="nav-link" href="#">
+									  Home 
+									</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="#">
+									  Features
+									</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="#">
+									  Pricing
+									</a>
+								  </li>
+								</ul>
+								<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+								  <li class="nav-item">
+									<a class="nav-link" href="#">
+									  Hi Kesiya Raj
+									</a>
+								  </li>
+								  <li class="nav-item">
+									<a class="nav-link" href="/">
+									  Logout
+									</a>
+								  </li>
+								</ul>
+							  </div>
+							</nav>
+							<div class="hero-image">
+					  <div class="hero-text">
+						<h1 style="font-size:40px">Shipping Port Management System</h1>
+					  </div>
+					</div>';
 echo "<h3> Start Date: ".$startdate." </h3>";
 echo "<h3> End Date: ".$enddate."</h3>";
 
@@ -50,9 +140,9 @@ $sql= "select Port_number, coalesce (count(*),0) as Total_ships,coalesce(sum(No_
 							and Number in (select Number from SHIPS_Operating_Seq where Operating_Seq like 'AL%') and Port_number ='".$port."'
 					group by Port_number;";
 
-
+					
 echo "<h4>Export Report </h4>";
-echo "<table border='1' >
+echo "<table border='1' class='table'>
     <tr>
     <th>Port Number</th>
     <th>Total Ships Export</th>
@@ -89,7 +179,7 @@ echo "</table>"."<br>";
 
 echo "<h4>Import Report</h4>";
 
-echo "<table border='1'>
+echo "<table border='1' class='table'>
     <tr>
     <th>Port Number</th>
 		<th>Total Ships Import</th>
@@ -123,7 +213,7 @@ echo "<table border='1'>
       }
 
 echo "</table>"."<br>";
-
+echo '</body>';
 
 
 ?>
@@ -142,7 +232,7 @@ th, td {
 tr:nth-child(even){background-color: #f2f2f2}
 
 th {
-  background-color: #4CAF50;
+  background-color: #4CAF50 !important;
   color: white;
 }
 </style>
